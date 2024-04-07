51b4ac885bf5cdb57f45d21216bff3d282449289
MikeYoulend <mikeyoulend@gmail.com>
2024-04-07 16:39:11 +0200

    Jquery

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-
-$("h1").css("color", "red")
+//Metodo sporco
+$("button").css("font-size", "40px")
+//metodo pulito
+$("h1").addClass("big-title margin-50")
+//puoi prendere anche solo il testo con .text
+$("button").html("<h1>Don't WATCH me</h1>")
 
